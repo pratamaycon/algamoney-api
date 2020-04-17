2f6974c0616ea89e5c14949d0dd00fa161d08821
Maycon Prata <mayconprata90@gmail.com>
2020-04-17 12:46:35 -0300

    retornando o meu primeiro resource categorias

---
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE categoria (
+codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(50) NOT NULL
+);
+
+INSERT INTO categoria(nome) values("Lazer");
+INSERT INTO categoria(nome) values("Alimentação");
+INSERT INTO categoria(nome) values("SuperMercado");
+INSERT INTO categoria(nome) values("Academia");
