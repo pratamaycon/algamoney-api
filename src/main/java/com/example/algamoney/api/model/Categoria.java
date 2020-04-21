792a301bf60d653a4e25e6ac53d983f5681200c6
Maycon Prata <mayconprata90@gmail.com>
2020-04-21 13:54:42 -0300

    exceptions api

--- src/main/java/com/example/algamoney/api/model/Categoria.java
+++ src/main/java/com/example/algamoney/api/model/Categoria.java
@@ -5,6 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
 
 @Entity
 @Table(name = "categoria")
@@ -13,6 +15,8 @@ public class Categoria {
 	@Id
 	@GeneratedValue( strategy = GenerationType.IDENTITY )
 	private Long codigo;
+	
+	@NotNull
 	private String nome;
 	
 	
