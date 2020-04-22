a32c1c619929d6031ba6a5459e65ef8c8feac38d
Maycon Prata <mayconprata90@gmail.com>
2020-04-21 22:44:56 -0300

    implementando cadastro de pessoa

---
+++ src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -0,0 +1,62 @@
+package com.example.algamoney.api.resource;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.algamoney.api.event.RecursoCriadoEvent;
+import com.example.algamoney.api.model.Pessoa;
+import com.example.algamoney.api.reposiory.PessoaRepository;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaResource {
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
+	@GetMapping
+	public List<Pessoa> listarPessoas(){
+		return pessoaRepository.findAll();
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<?> buscarPeloId(@PathVariable Long codigo){
+		
+		Optional<Pessoa> pessoa = pessoaRepository.findById(codigo);
+		
+		if (!pessoa.isPresent()) {
+			ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(pessoa);
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Pessoa> criarPessoa(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response){
+		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
+	}
+
+}
